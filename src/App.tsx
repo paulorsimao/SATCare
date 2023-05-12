6f222f5bc43aec418a29c2cc536e808c342cc7f6
darabas <gmd.darabas@hotmail.com>
2023-05-12 18:00:43 -0300

    utiliza variavel de ambiente para definir a porta do vite; eslint com tabs

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,15 @@ import logo from "/logo.jpg";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <div>
-        {/* <a href="https://vitejs.dev" target="_blank"> */}
-        <img src={logo} style={{ width: '40vh', height: 'auto', borderRadius: '50px' }} className="logo" alt="Vite logo" />
-        {/* </a> */}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div>
+				{/* <a href="https://vitejs.dev" target="_blank"> */}
+				<img src={logo} style={{ width: '40vh', height: 'auto', borderRadius: '50px' }} className="logo" alt="Vite logo" />
+				{/* </a> */}
+			</div>
+		</>
+	);
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default function({ command, mode }) {
+	const env = loadEnv(mode, process.cwd(), "");
+
+	return defineConfig({
+		server: {
+			port: +env.VITE_PORT
+		},
+		plugins: [react()],
+	})
+}
