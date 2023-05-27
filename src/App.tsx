940b1d948d1077548fc50e21ce49af60b7f6a06d
Paulo Simão <112284509+paulorsimao@users.noreply.github.com>
2023-05-27 15:47:42 -0300

    Tela de cadastro de usuarios

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import "./App.css";
-import { ConfigProvider, theme } from "antd";
-import themeOptions from './antd.theme.json'
+import { ConfigProvider, } from "antd";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import withAdminLayout from "./layouts/admin-layout/withAdminLayout";
+import cadastro from "./features/usuarios/components/cadastro-usuarios/cadastro";
 
 function App() {
 	return (
-		<ConfigProvider theme={{
-			...themeOptions,
-			algorithm: theme.darkAlgorithm,
-		}}>
+		<ConfigProvider>
 			<Router>
 				<Routes>
 					<Route path='/' Component={() => withAdminLayout('Home')} />
 					<Route path='/home' Component={() => withAdminLayout('Home')} />
 					<Route path='/usuarios' Component={() => withAdminLayout('Usuários')} />
 					<Route path='/profissionais' Component={() => withAdminLayout('Profissionais')} />
+					<Route path='/cadastro' Component={cadastro} />
 				</Routes>
 			</Router>
 		</ConfigProvider>
---
+++ src/features/usuarios/components/cadastro-usuarios/cadastro.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Button, Checkbox, Form, Input } from 'antd';
+
+const onFinish = (values: any) => {
+  console.log('Success:', values);
+};
+
+const onFinishFailed = (errorInfo: any) => {
+  console.log('Failed:', errorInfo);
+};
+
+const cadastro: React.FC = () => (
+  <Form
+    name="basic"
+    labelCol={{ span: 8 }}
+    wrapperCol={{ span: 16 }}
+    style={{ maxWidth: 600 }}
+    initialValues={{ remember: true }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+  >
+    <Form.Item
+      label="Nome"
+      name="nome"
+      rules={[{ required: true, message: 'Por favor insira o nome!' }]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item
+      label="E-mail"
+      name="email"
+      rules={[{ required: true, message: 'Por favor insira o e-mail!' }]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item
+      label="Telefone"
+      name="telefone"
+      rules={[{ required: true, message: 'Por favor insira o telefone!' }]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item
+      label="Password"
+      name="password"
+      rules={[{ required: true, message: 'Please input your password!' }]}
+    >
+      <Input.Password />
+    </Form.Item>
+
+    <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+      <Checkbox>Remember me</Checkbox>
+    </Form.Item>
+
+    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+      <Button type="primary" htmlType="submit">
+        Cadastrar
+      </Button>
+    </Form.Item>
+
+    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+      <Button type="primary" htmlType="submit">
+        Entrar
+      </Button>
+    </Form.Item>
+  </Form>
+);
+
+export default cadastro;
