55e986192dac6b0a942df88570ad7f8747d7ec06
Paulo Simão <112284509+paulorsimao@users.noreply.github.com>
2023-05-28 17:25:02 -0300

    Deixei o cadastro mais bonitinho

--- src/features/usuarios/components/cadastro-usuarios/cadastro.tsx
+++ src/features/usuarios/components/cadastro-usuarios/cadastro.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Form, Input } from 'antd';
+import { Button, Form, Input, Row, Col } from 'antd';
 import './styled.css'
 
 const onFinish = (values: any) => {
@@ -12,57 +12,63 @@ const onFinishFailed = (errorInfo: any) => {
 
 const cadastro: React.FC = () => (
   <div id= 'blocoCadastro'>
-  <Form
-    name="basic"
-    labelCol={{ span: 8 }}
-    wrapperCol={{ span: 16 }}
-    style={{ maxWidth: 600 }}
-    initialValues={{ remember: true }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <Form.Item
-      label="Nome"
-      name="nome"
-      rules={[{ required: true, message: 'Por favor insira o nome!' }]}
+    <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      style={{ maxWidth: 600 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
     >
-      <Input />
-    </Form.Item>
+      <Form.Item
+        label="Nome"
+        name="nome"
+        rules={[{ required: true, message: 'Por favor insira o nome!' }]}
+      >
+        <Input />
+      </Form.Item>
 
-    <Form.Item
-      label="E-mail"
-      name="email"
-      rules={[{ required: true, message: 'Por favor insira o e-mail!' }]}
-    >
-      <Input />
-    </Form.Item>
+      <Form.Item
+        label="E-mail"
+        name="email"
+        rules={[{ required: true, message: 'Por favor insira o e-mail!' }]}
+      >
+        <Input />
+      </Form.Item>
 
-    <Form.Item
-      label="Telefone"
-      name="telefone"
-      rules={[{ required: true, message: 'Por favor insira o telefone!' }]}
-    >
-      <Input />
-    </Form.Item>
+      <Form.Item
+        label="Telefone"
+        name="telefone"
+        rules={[{ required: true, message: 'Por favor insira o telefone!' }]}
+      >
+        <Input />
+      </Form.Item>
 
-    <Form.Item
-      label="Senha"
-      name="senha"
-      rules={[{ required: true, message: 'Por favor insira a senha!' }]}
-    >
-      <Input.Password />
-    </Form.Item>
+      <Form.Item
+        label="Senha"
+        name="senha"
+        rules={[{ required: true, message: 'Por favor insira a senha!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
 
-    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-      <Button id="btnCadastro" htmlType="submit">
-        CADASTRAR
-      </Button>
-      <Button id="btnEntrar" htmlType="submit">
-        ENTRAR
-      </Button>
-    </Form.Item>
-  </Form>
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Row justify="center">
+          <Col>
+            <Button id="btnCadastro" htmlType="submit">
+              CADASTRAR
+            </Button>
+          </Col>
+          <Col>
+            <Button id="btnEntrar" htmlType="submit">
+              ENTRAR
+            </Button>
+          </Col>
+        </Row>
+      </Form.Item>
+    </Form>
   </div>
 );
 
